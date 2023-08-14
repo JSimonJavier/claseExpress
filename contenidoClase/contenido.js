a937c8b32bd395a28ff03f114a12f4308c3f022f
Javier Simon <simonjavier.fullstack@gmail.com>
2023-08-14 10:15:08 -0300

    Agregue la carperta contenido y el index

---
+++ contenidoClase/contenido.js
@@ -0,0 +1,10 @@
+/*
+? EXPRESS
+  -Es un framework que perimite creacion de servidores y manejo de rutas, solicitudes y respuestas http.
+  -Es facil de usar para node.js -> desarrolladores creen aplicacion web de forma rapida y sencilla
+  -Construir nuestra API(aplication programminf interfaces)
+
+  
+
+  
+*/
